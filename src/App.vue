3564f681dd6fb280a5ee93721d3cc3fd6dcf6a04
Reno Ardian Syaputra <reno.ardian@aqi.co.id>
2024-10-22 20:01:30 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+// tidak dipakai
+const userName: string = 'reno'
+
+// umur
+const age: number = 18
+const objAddress: {
+  street: string
+  houseNumber: number
+  city: string
+  country: string
+  state: string
+} = {
+  street: 'xian',
+  houseNumber: 1,
+  city: 'beijing',
+  country: 'china',
+  state: 'shanghai',
+}
+console.log('userName:', userName)
+console.log('age:', age)
+console.log('objAddress:', objAddress)
+console.log('baris baru untracked git')
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/" class="bg-red-500 text-white">Home</RouterLink>
+        <RouterLink to="/about" class="bg-green-500 text-white">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
